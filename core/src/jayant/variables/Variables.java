141b9262ebd03c9ffa5df60be9dc43ec3d3708a1
Jayant <goyal151002@gmail.com>
2025-07-29 22:48:20 +0530

    Created the variable file

---
+++ core/src/jayant/variables/Variables.java
@@ -0,0 +1,11 @@
+package jayant.variables;
+
+public class Variables {
+    public static void main(String[] args){
+
+        System.out.println("--variables--");
+        int a = 10;
+        System.out.println("Value of a : " + a);
+
+    }
+}
